584adb7f213a7f89203ece45a33798447e166008
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 16:19:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,9 +61,11 @@ class Owner
   end
 
   def sell_pets
-    pets.each do |pet|
-      pet.mood = "nervous"
+    pets.each do |species, animals|
+      animals.each do |animal
+      animal.mood = "nervous"
     end
+    animals.clear
   end
 
 
