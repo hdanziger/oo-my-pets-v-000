6d024ba26566d63f75201a1680c5753d71dba072
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 15:46:56 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,20 @@
 class Owner
   # code goes here
+  owners = []
   attr_accessor :pets, :name
+
+  def self.reset_all
+    owners.clear
+  end
+
+  def self.count
+    owners.size
+  end
 
   def initialize(owner)
     @name = name
     @pets= {:fishes => [], :dogs => [], :cats => []}
-    owners << self 
+    owners << self
   end
 
 
