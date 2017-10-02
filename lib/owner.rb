26756e7086a732f840b03819fa2608b5633b3f9f
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 15:33:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,6 +2,6 @@ class Owner
   # code goes here
 
   def initialize(owner)
-    @pets= pets: {:fishes => [], :dogs => [], :cats => []}
+    @pets= {:fishes => [], :dogs => [], :cats => []}
   end
 end
